35677052fb9910d87648fd455730fc31eb677ee9
lirui <1390929247@qq.com>
2018-10-14 21:41:37 +0800

    diaf

--- testforgit/src/com/itheima/TestGit.java
+++ testforgit/src/com/itheima/TestGit.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test...git....");
+        System.out.println(",,,,,");
     }
 }
